28200d5db449c7168003246dae402c72850985b5
Will Gundy <willgunderson503@gmail.com>
2022-07-25 10:22:38 -0700

    get basics of file setup

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client';
+import App from './components/App';
+
+const container = document.getElementById('app');
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
