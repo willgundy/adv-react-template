a660557ec65a1441c739c84718cc4691d4c0786a
Will Gundy <willgunderson503@gmail.com>
2022-07-25 17:40:58 -0700

    reinstall dependencies, can't get routing to work

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,18 @@
+import { Navigate, Route, Router, Routes } from "react-router-dom";
+import About from "./About/About";
+import Home from "./Home/Home";
+import List from "./List/List";
+import Layout from "./Page/Layout";
+
 export default function App() {
-    return <div>Hello World</div>
+    <Router>
+        <Routes>
+            <Route path='/' element={<Layout />}>
+                <Route index element={<Home />}/>
+                <Route path='list' element={<List />}/>
+                <Route path='about' element={<About />}/>
+                <Route path='*' element={<Navigate to='/' replace />}/>
+            </Route>
+        </Routes>
+    </Router>
 }
---
+++ src/components/Page/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      Header
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='list'>List</Link>
+        <Link to='about'>About</Link>
+      </nav>
+    </header>
+  )
+}
