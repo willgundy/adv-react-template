ecd01a07e1d1346e0045da8d5de4ba56a0a79854
Will Gundy <willgunderson503@gmail.com>
2022-07-25 18:01:56 -0700

    moving main back to hello world

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,7 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
-import About from "./About/About";
-import Home from "./Home/Home";
-import List from "./List/List";
-import Layout from "./Page/Layout";
+
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="list" element={<List />} />
-          <Route path="about" element={<About />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Route>
-      </Routes>
-    </Router>
+    <div>Hello World! </div>
   );
 }
