7d6e1ba3c11128a3e2b9d352ef829bf20f5ed69a
Will Gundy <willgunderson503@gmail.com>
2022-07-25 17:43:11 -0700

    got hello world working again

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,19 @@ import Home from "./Home/Home";
 import List from "./List/List";
 import Layout from "./Page/Layout";
 
+// export default function App() {
+//     <Router>
+//         <Routes>
+//             <Route path='/' element={<Layout />}>
+//                 <Route index element={<Home />}/>
+//                 <Route path='list' element={<List />}/>
+//                 <Route path='about' element={<About />}/>
+//                 <Route path='*' element={<Navigate to='/' replace />}/>
+//             </Route>
+//         </Routes>
+//     </Router>
+// }
+
 export default function App() {
-    <Router>
-        <Routes>
-            <Route path='/' element={<Layout />}>
-                <Route index element={<Home />}/>
-                <Route path='list' element={<List />}/>
-                <Route path='about' element={<About />}/>
-                <Route path='*' element={<Navigate to='/' replace />}/>
-            </Route>
-        </Routes>
-    </Router>
+  return <div>Hello React Webpack Project</div>;
 }
