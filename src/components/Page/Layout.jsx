e46979e4870b55599b92f40f818bb059ae036e1d
Will Gundy <willgunderson503@gmail.com>
2022-07-25 18:22:16 -0700

    added a file for the header and moved header component there

--- src/components/Page/Layout.jsx
+++ src/components/Page/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Header from './Header';
+import Header from './Header/Header.jsx';
 import styles from './Layout.css';
 import './global.css';
 import './reset.css';
