605e4246a2b37bb2bd39285578d39d0f8cae0082
Will Gundy <willgunderson503@gmail.com>
2022-07-25 18:20:29 -0700

    added reset and global file to Layout component

--- src/components/Page/Layout.jsx
+++ src/components/Page/Layout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
+import styles from './Layout.css';
+import './global.css';
+import './reset.css';
+
 
 export default function Layout() {
   return (
-    <div>
+    <div className={styles.Layout}>
       <Header />
       <main>
         <Outlet />
