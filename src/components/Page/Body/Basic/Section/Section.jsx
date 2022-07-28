9897a749b4e913eb1db7ab6abe152ddf8ff42270
Will Gundy <willgunderson503@gmail.com>
2022-07-27 18:22:49 -0700

    fixed width of section

--- src/components/Page/Body/Basic/Section/Section.jsx
+++ src/components/Page/Body/Basic/Section/Section.jsx
@@ -2,7 +2,7 @@ import styles from './Section.css';
 
 export default function Section({ header, subText, width, height, children }) {
   return (
-    <section className={styles.Section} style={{ width, height }}>
+    <section className={styles.Section} style={{ minWidth: width, height }}>
       {header && <h1>{header}</h1>}
       {subText && <h4>{subText}</h4>}
       {children}
