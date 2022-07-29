ab2902d2ae7580ccb41f196b9f77a3a07cb0d863
Will Gundy <willgunderson503@gmail.com>
2022-07-29 12:56:20 -0700

    added pokedex state setting

---
+++ src/components/State/State/pokedex.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { getPokedex } from '../Services/pokedex-service';
+
+export default function pokedex() {
+    const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
+    const [count, setCount] = useState(0);
+
+    const fetchPokedex = async () => {
+        const res = await getPokedex();
+
+        if (res.data) {
+            setData(res.data.results);
+            setCount(res.data.count);
+            setError(null);
+        }
+
+        if (res.error) {
+            setError(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchPokedex();
+    }, []);
+
+    return { error, data, count };
+}
