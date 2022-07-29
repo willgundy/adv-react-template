cff9657a9d7a062dfea09f7d2b4f55d6a33e0351
Will Gundy <willgunderson503@gmail.com>
2022-07-29 15:15:57 -0700

    got search params setup

--- src/components/Page/Body/List/SearchBar.jsx
+++ src/components/Page/Body/List/SearchBar.jsx
@@ -1,19 +1,44 @@
 import {  InputControl } from '../Form/FormControls/FormControls.jsx';
 import Button from '../Basic/Button/Button';
 import styles from './List.css';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 export default function SearchBar() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const pokemon = searchParams.get('pokemon') || '';
+    const type = searchParams.get('type') || '';
+  
+    const [search, setSearch] = useState({ pokemon, type });
+  
+    useEffect(() => {
+        setSearch({ pokemon, type });
+    }, [pokemon, type]);
+  
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSearchParams(search);
+    };
+  
+    const handleChange = ({ target: { name, value } }) => {
+        setSearch((search) => ({ ...search, [name]: value }));
+    };
+
     return (
-        <form >
+        <form onSubmit={handleSubmit}>
             <div className={styles.Filters}>
                 <InputControl
                     name="pokemon"
                     placeholder="Search by Pokemon Name"
-                    label="name"
+                    label="pokemon"
+                    value={search.pokemon}
+                    onChange={handleChange}
                 />
                 <InputControl
                     label="type"
                     name="type"
+                    value={search.type}
+                    onChange={handleChange}
                 />
             </div>
             <Button text={'Search Pokedex'}/>
