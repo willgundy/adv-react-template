57f88a62856750a78c068c7c1f152f040747902f
Will Gundy <willgunderson503@gmail.com>
2022-08-04 16:19:30 -0700

    trying to fix CI for netlify

--- src/components/State/Services/client.js
+++ src/components/State/Services/client.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://rbbzxmskagprovqccrmk.supabase.co';
-const SUPABASE_KEY = process.env.SUPABASE_CLIENT_API_KEY;
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
+const SUPABASE_KEY = process.env.REACT_APP_SUPABASE_KEY;
 export const client = createClient(SUPABASE_URL, SUPABASE_KEY);
 
